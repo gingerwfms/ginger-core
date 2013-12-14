163ad347d0bb9bae6314746c29efd6587974c085
Codeliner <kontakt@codeliner.ws>
2013-12-14 18:56:46 +0100

    Fix namespace problem in Definition

--- src/GingerCore/Definition.php
+++ src/GingerCore/Definition.php
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-namespace GingerCore\;
+namespace GingerCore;
 
 /**
  * Definition class
